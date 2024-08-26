748af20c0db87a290b18c3d4da6c8b3b31efe6cb
jaysonamati <kingoinajayson@gmail.com>
2024-08-26 21:19:29 +0300

    :mod add language servers for my languages

--- init.lua
+++ init.lua
@@ -572,7 +572,11 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        zls = {},
+        hls = {},
+        -- agda_ls = {},
+        -- idris2_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
